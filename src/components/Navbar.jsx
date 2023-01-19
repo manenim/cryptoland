eb8925de6f2165df758c4fffcdbef0eaacc17567
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-01-19 11:18:26 +0100

    fix: implement bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <div className="logo-container">
             <Avatar src = {icon} size = "large" />
             <Typography.Title level={2} className = "logo">
-                <Link to = "/">Cryptoland</Link>
+                <Link to = "/cryptoland">Cryptoland</Link>
             </Typography.Title>
             <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
       </div>
